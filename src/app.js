92074b33dadb703dc7b7d64d25053c600f4861da
maekelly <112394441+maekelly@users.noreply.github.com>
2022-12-11 19:38:17 +0000

    Added dark theme button

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+function changeTheme() {
+  let body = document.querySelector("body");
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    document.getElementById("sun").src = "./media/sun.png";
+  } else {
+    body.classList.add("dark");
+    document.getElementById("sun").src = "./media/moon.png";
+  }
+}
 
+let themeBtn = document.querySelector(".theme");
+themeBtn.addEventListener("click", changeTheme);
