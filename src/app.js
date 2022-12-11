b6e9ce72e4d468e9e978cb832d9f329914a16d2d
maekelly <112394441+maekelly@users.noreply.github.com>
2022-12-11 21:52:21 +0000

    Minor alterations, began work on API call

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function searchCity() {
+  console.log(searchInput.value);
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}`;
+  console.log(apiUrl);
+}
+
+let apiKey = "2c8a3dfad180of4f0ft83a44b4afcc97";
+let searchInput = document.querySelector("#search-input");
+let searchBtn = document.querySelector("#search-btn");
+let citySearch = document.querySelector("#search-form");
+let cityShown = document.querySelector("#cityShown");
+let tempShown = document.querySelector("#temperatureShown");
+
+citySearch.addEventListener("submit", searchCity);
+
+//Theme Changing button
 function changeTheme() {
   let body = document.querySelector("body");
   if (body.classList.contains("dark")) {
